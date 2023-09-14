c2bf6e8e08c61ec270a99bb030bb67187afbe501
Aaron Moreno <camaron1111@gmail.com>
2023-09-14 03:18:13 +0000

    Test Commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Website</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <?php
+        // PHP code can go here
+        $message = "Welcome to my PHP website! Test";
+        echo "<p>$message</p>";
+    ?>
+</body>
+</html>
